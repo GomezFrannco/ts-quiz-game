ae1e20a30a184e46b8e4eec9a44b693405438d33
GomezFrannco <frangomez.baires@gmail.com>
2022-06-17 17:35:18 -0300

    [Updt] database funct: return was updated

--- src/config/mongoose.config.ts
+++ src/config/mongoose.config.ts
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 
-export async function connectMongo(url: string) {
-  await mongoose.connect(url, (err) => {
+export async function connectMongo(url: string): Promise<void> {
+  return await mongoose.connect(url, (err): void => {
     if (!err) {
-      console.log({
+      return console.log({
         message: "Connected",
       });
     } else {
-      console.error({
+      return console.error({
         message: err.message,
       });
     }
   });
 }
+
+connectMongo("mongodb+srv://sukiDev:Password123%C2%A1@cluster0.8kpsl.mongodb.net/test");
